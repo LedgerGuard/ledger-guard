c990feb3bea5c0ceb7d371d5a0dbe106c01abc27
zchothia <zaheer.chothia@gmail.com>
2023-09-17 04:02:36 +0200

    Replace smart contract demo with working example
    Tracing shows we only need a subset of the full oz sources:
    ```shell
    $ git add ./{index,utils}.js
    $ git add ./ExpiryHelper.sol
    $ git add ./FeeHelper.sol
    $ git add ./HederaResponseCodes.sol
    $ git add ./HederaTokenService.sol
    $ git add ./IHederaTokenService.sol
    $ git add ./KeyHelper.sol
    $ git add ./oz-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol
    $ git add ./oz-contracts/contracts/token/ERC20/IERC20.sol
    $ git add ./TokenReceiver.sol
    $ git add ./TokenSender.sol
    ```

---
+++ smart/TokenSender.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.6.0 <0.9.0;
+
+import './HederaResponseCodes.sol';
+import './IHederaTokenService.sol';
+import './HederaTokenService.sol';
+import './ExpiryHelper.sol';
+
+import './oz-contracts/contracts/token/ERC20/IERC20.sol';
+import './oz-contracts/contracts/token/ERC20/extensions/IERC20Metadata.sol';
+
+contract TokenSender is ExpiryHelper {
+    mapping(address => mapping(address => bool)) exhausted;
+
+    // create a fungible Token with no custom fees,
+    function createFungible(
+        string memory name,
+        string memory symbol,
+        uint initialSupply,
+        uint decimals,
+        uint32 autoRenewPeriod
+    ) external payable returns (address createdTokenAddress) {
+
+        IHederaTokenService.HederaToken memory token;
+        token.name = name;
+        token.symbol = symbol;
+        token.treasury = address(this);
+
+        // create the expiry schedule for the token using ExpiryHelper
+        token.expiry = createAutoRenewExpiry(address(this), autoRenewPeriod);
+
+        // call HTS precompiled contract, passing initial supply and decimals
+        (int responseCode, address tokenAddress) =
+                    HederaTokenService.createFungibleToken(token, initialSupply, decimals);
+
+        if (responseCode != HederaResponseCodes.SUCCESS) {
+            revert ();
+        }
+
+        createdTokenAddress = tokenAddress;
+    }
+
+    function transferPrecompile(address tokenId, address receiver, int64 amount) external {
+        if (exhausted[address(this)][receiver]) {
+            revert ("Vote exhausted by prior selection");
+        }
+
+        int response = HederaTokenService.transferToken(tokenId, address(this), receiver, amount);
+
+        if (response != HederaResponseCodes.SUCCESS) {
+            revert ("Transfer Failed");
+        }
+
+        exhausted[address(this)][receiver] = true;
+    }
+
+    // Transfer token from this contract to the recipient
+    function transferERC(address token, address recipient, uint256 amount) public {
+        IERC20(token).transfer(recipient, amount);
+    }
+
+    fallback () external{}
+
+}
